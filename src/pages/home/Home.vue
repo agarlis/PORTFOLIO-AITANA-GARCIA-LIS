01cad3c8acc57781860b7d5bdaea7d928fabaab6
Aitana Garcia <aitanagarcialis@wifialu9-127.wi-fi.upv.es>
2026-02-26 12:45:58 +0100

    Estilos y tifografias de la portada

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -7,27 +7,27 @@
 
   <div class="">
     <div class="">
-      <span class="">Graphic Designer</span>
+      <span class="rama">Graphic Designer</span>
       <br>
-      <span class="">Portfolio</span>
+      <span class="portfolio">Portfolio</span>
     </div>
-    <h1 class="">
+    <h1 class="nom">
       Aitana Garcia
     </h1>
-    <div class="">
+    <div class="frase">
     <p>Diseño que conecta</p>
   </div>
   </div>
 <div class="flex gap-8">
-<Button>
+<Button class="Button">
   <RouterLink to="/Trabajos">Trabajos</RouterLink>
 </Button>
 
-<Button >
+<Button class="Button">
   <RouterLink to="/SobreMi">Sobre mi</RouterLink>
 </Button>
 
-<Button>
+<Button class="Button">
   <RouterLink to="/Contactos">Contacto</RouterLink>
 </Button>
 </div>
@@ -35,5 +35,40 @@
 </template>
 
 <style scoped>
+
+.rama {
+  font-family: bodonimodaitalic;
+  font-weight: 400;
+}
+
+.portfolio {
+  font-family: montserrat;
+  font-weight: 700;
+  text-transform: uppercase;
+}
+
+.nom{
+      font-family: montserrat;
+      font-weight: 900;
+      text-transform: uppercase;
+      line-height: 0.9;
+}
+
+.frase {
+  font-family: montserrat;
+  font-weight: 200; 
+  line-height: 0.9;
+}
+
+.Button{
+font-family: montserrat;
+font-weight: 200; 
+}
+
+.Button:hover{
+font-family: montserrat;
+font-weight: 500; 
+}
+
 
 </style>
