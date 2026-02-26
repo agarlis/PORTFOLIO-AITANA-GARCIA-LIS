4dde4e0e657ea368abd573a39a95b9ae7950f067
Aitana Garcia <aitanagarcialis@MacBook-Pro-de-Aitana-3.local>
2026-02-26 17:17:30 +0100

    estructura portada

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,37 +1,50 @@
 <script lang="ts" setup>
 
+import LogoGarlisDesing from '@/components/LogoGarlis.Desing.vue'
 
 </script>
 
 <template>
+    <section class="relative overflow-hidden">
+        <nav class=" flex justify-between items-center px-8 text-sm uppercase tracking-widest">
+            <LogoGarlisDesing estilos="w-[6rem]"/>
+            <div class="flex gap-8 tracking-widest">
+                <Button class="Button">
+                    <RouterLink to="/Trabajos">Trabajos</RouterLink>
+                </Button>
 
-  <div class="">
-    <div class="">
-      <span class="rama">Graphic Designer</span>
-      <br>
-      <span class="portfolio">Portfolio</span>
+                <Button class="Button">
+                    <RouterLink to="/SobreMi">Sobre mi</RouterLink>
+                </Button>
+
+                <Button class="Button">
+                    <RouterLink to="/Contactos">Contacto</RouterLink>
+                </Button>
+            </div>
+        </nav>
+
+  <div class="flex flex-col justify-center min-h-screen px-8 md:px-10">
+    <div class="flex justify-between text-xl">
+      <span class="rama text-3xl">Graphic Designer</span>
+            <img 
+              src="/image/fotoaitana.png"
+              alt=""
+              class="absolute right-[20%] w-[350px]"
+            />
+      <span class="portfolio text-3xl">Portfolio</span>
+      
     </div>
-    <h1 class="nom">
-      Aitana Garcia
-    </h1>
-    <div class="frase">
+        <h1 class="nombre mt-4 text-[9rem]">
+        Aitana<br>Garcia
+        </h1>
+    <div 
+    class="frase mt-4 text-xl">
     <p>Diseño que conecta</p>
   </div>
+
   </div>
-<div class="flex gap-8">
-<Button class="Button">
-  <RouterLink to="/Trabajos">Trabajos</RouterLink>
-</Button>
 
-<Button class="Button">
-  <RouterLink to="/SobreMi">Sobre mi</RouterLink>
-</Button>
-
-<Button class="Button">
-  <RouterLink to="/Contactos">Contacto</RouterLink>
-</Button>
-</div>
-
+    </section>
 </template>
 
 <style scoped>
@@ -47,7 +60,7 @@
   text-transform: uppercase;
 }
 
-.nom{
+.nombre{
       font-family: montserrat;
       font-weight: 900;
       text-transform: uppercase;
