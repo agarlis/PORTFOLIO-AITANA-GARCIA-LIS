53160b0ee444ac31135653a7bac10d3219fa7db2
Aitana Garcia <aitanagarcialis@wifialu9-127.wi-fi.upv.es>
2026-02-26 12:10:03 +0100

    Información portada + botones

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,8 +5,32 @@
 
 <template>
 
-<h1>PORTADA</h1>
+  <div class="">
+    <div class="">
+      <span class="">Graphic Designer</span>
+      <br>
+      <span class="">Portfolio</span>
+    </div>
+    <h1 class="">
+      Aitana Garcia
+    </h1>
+    <div class="">
+    <p>Diseño que conecta</p>
+  </div>
+  </div>
+<div class="flex gap-8">
+<Button>
+  <RouterLink to="/Trabajos">Trabajos</RouterLink>
+</Button>
 
+<Button >
+  <RouterLink to="/SobreMi">Sobre mi</RouterLink>
+</Button>
+
+<Button>
+  <RouterLink to="/Contactos">Contacto</RouterLink>
+</Button>
+</div>
 
 </template>
 
