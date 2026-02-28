0a62d4b834950eff7221dad5d26e57996e1e2640
Aitana Garcia <aitanagarcialis@MacBook-Pro-de-Aitana-3.local>
2026-03-01 00:49:46 +0100

    Diseño portada FINALIZADA

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,18 +1,21 @@
 <script lang="ts" setup>
-
+import { ref } from "vue"
+import { Menu } from 'lucide-vue-next';
 import LogoGarlisDesing from '@/components/LogoGarlis.Desing.vue'
 
+
+const menuAbierto = ref(false)
 </script>
 
 <template>
     <section class="relative min-h-screen bg-[#F3EBDD] overflow-hidden ">
-        <nav class="absolute top-0 left-0 w-full flex justify-between items-center px-8 text-sm uppercase tracking-widest">
+        <nav class="absolute top-0 left-0 w-full flex justify-between items-center px-8 py-5 text-sm uppercase tracking-widest transition-all">
           
             <div class="text-[#5E4B3C]">
                 <LogoGarlisDesing estilos="text-[#5E4B3C] sm:w-[4rem] md:w-[6rem] lg:w-[8rem] transition-all"/>
             </div>
 
-            <div class="flex gap-8 tracking-widest">
+            <div class="hidden md:flex gap-8 tracking-widest">
                 <Button class="Button">
                     <RouterLink to="/Trabajos">Trabajos</RouterLink>
                 </Button>
@@ -25,24 +28,39 @@ import LogoGarlisDesing from '@/components/LogoGarlis.Desing.vue'
                     <RouterLink to="/Contactos">Contacto</RouterLink>
                 </Button>
             </div>
+
+            <button 
+                class="iconoboton block flex align-center justify-center md:hidden lg:hidden text-3xl text-[#5E4B3C]"
+                @click="menuAbierto = !menuAbierto">
+                    <Menu class="w-5 h-5" />
+            </button>
         </nav>
+
+    <div 
+        v-if="menuAbierto"
+        class="menu absolute top-[80px] tracking-widest right-8 flex flex-col gap-4 bg-white/20 backdrop-blur-md p-6 rounded-2xl shadow-lg md:hidden z-50">
+            <RouterLink to="/Trabajos" @click="menuAbierto = false">Trabajos</RouterLink>
+            <RouterLink to="/SobreMi" @click="menuAbierto = false">Sobre mi</RouterLink>
+            <RouterLink to="/Contactos" @click="menuAbierto = false">Contacto</RouterLink>
+    </div>
 
   <div class="bg-[#F3EBDD] flex flex-col justify-center min-h-screen px-8 md:px-10">
     <div class="flex justify-between text-xl text-[#5E4B3C]">
-      <span class="rama text-3xl">Graphic Designer</span>
+      <span class="rama sm:text-1xl md:text-2xl lg:text-3xl transition-all">Graphic Designer</span>
             <img 
               src="/image/fotoaitana.png"
               alt=""
-              class="absolute right-[19%] w-[450px]"
+              class="absolute right-[19%] w-[0px] sm:w-[0px] md:w-[320px] lg:w-[450px] object-cover z-10 duration-500 ease-out hover:scale-125 transition-all"
             />
-      <span class="portfolio text-3xl">Portfolio</span>
-      
+      <span class="portfolio text-1xl sm:text-1xl md:text-2xl lg:text-3xl transition-all">Portfolio</span>
     </div>
-        <h1 class="nombre mt-4 text-[9rem] text-[#3A2A20]">
+
+    <h1 class="nombre mt-4 text-7xl sm:text-7xl md:text-8xl lg:text-[9rem] text-[#3A2A20] transition-all">
         Aitana<br>Garcia
-        </h1>
+    </h1>
+
     <div 
-    class="frase mt-4 text-xl text-[#5E4B3C]">
+        class="frase mt-4 text-base sm:text-base md:text-lg lg:text-xl text-[#5E4B3C] transition-all">
     <p>Diseño que conecta</p>
   </div>
 
@@ -52,6 +70,41 @@ import LogoGarlisDesing from '@/components/LogoGarlis.Desing.vue'
 </template>
 
 <style scoped>
+
+.iconoboton {
+    align-items: center;
+    justify-content: center;
+    width: 30px;
+    height: 120%;
+    border-radius: 50px;
+    transition: all 0.6 ease;
+    position: relative;
+    background: rgba(255, 255, 255, 0.15);
+    backdrop-filter: blur(12px);
+    -webkit-backdrop-filter: blur(12px);
+    border: 1px solid rgba(255, 255, 255, 0.3);
+    box-shadow: 0 8px 20px rgba(0, 0, 0, 0.15);
+    transition: all 0.3s ease;
+    cursor: pointer;
+}
+
+
+.iconoboton:hover {
+    font-family: montserrat;
+    font-weight: 800;
+    background: rgba(255, 255, 255, 0.25);
+    transform: scale(1.05);
+    box-shadow: 0 8px 8px rgba(0, 0, 0, 0.15);
+}
+
+.menu{
+    font-family: montserrat;
+    font-weight: 400;
+}
+.menu:hover{
+
+}
+
 
 .rama {
     font-family: bodonimodaitalic;
@@ -79,7 +132,7 @@ import LogoGarlisDesing from '@/components/LogoGarlis.Desing.vue'
 
 .Button{
     font-family: montserrat;
-    font-weight: 200;
+    font-weight: 500;
     width: 100px;
     height: 120%;
     border-radius: 50px;
@@ -101,7 +154,7 @@ import LogoGarlisDesing from '@/components/LogoGarlis.Desing.vue'
 
 .Button:hover{
     font-family: montserrat;
-    font-weight: 500;
+    font-weight: 800;
     background: rgba(255, 255, 255, 0.25);
     transform: scale(1.05);
     box-shadow: 0 8px 8px rgba(0, 0, 0, 0.15);
